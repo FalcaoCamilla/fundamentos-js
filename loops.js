4c4c5f18a07d35e0b90914dddd5fb7d6136acf52
FalcaoCamilla <millafalcao.acad@gmail.com>
2022-05-12 12:26:43 -0300

    loops

---
+++ loops.js
@@ -0,0 +1,48 @@
+function numeroPositivo(num){
+    let resultado;
+
+    if (num<0){
+        resultado = false;
+    }else{
+        resultado = true;
+    }
+    return resultado;
+}
+
+numeroPositivo(2);
+
+function numPositivo(num){
+    const ehNegativo = num < 0;
+
+    if(ehNegativo){
+        return false;
+    }
+    return true;
+}
+
+function numerPositivo(num){
+    const Negativo=num<0;
+    const maiorQueDez=num>10;
+
+    if(Negativo){
+        return "Esse número é negativo";
+    } else if(!Negativo && maiorQueDez){
+        return "Esse número é positivo e maior que 10!"
+    }
+    return "Esse número é positivo!";
+}
+
+//SWITCH
+
+function getAnimal(id){
+    switch(id){
+        case 1:
+            return"cão";
+        case 2:
+            return"gato";
+        case 3:
+            return"pássaro";
+        default: //sempre precisa de um valor default, pois será o return para respostas inesperadas
+            return "peixe";
+    }
+}
