3c8d5027896f8437d620d9f204e340e269dfeddd
FalcaoCamilla <millafalcao.acad@gmail.com>
2022-08-03 09:38:21 -0300

    arrow e objetos

--- arrow.js
+++ arrow.js
@@ -1,10 +1,22 @@
+//function
+function nomeFunc(){
+    return "Hello World"
+}
+
 const helloWorld1 = function(){
     return "Hello World";
 }
-const helloWorld2 = () => {
+
+//arrow function
+const helloWorld2 = () => { //const para indicar o nome, parênteses para indicar função, arrow para indicar o comportamento
     return "Hello World"
 }
-const helloWorld3 = () => "Hello World"
+
+const helloWorld3 = () => "Hello World" //arrow para indicar o retorno direto
+
+const nomeFuncArrowArguments = () => arguments //arrow functions não retorna arguments
+
+
 
 //arrow function não faz hoisting, é preciso declarar ela antes
 
--- objetos.js
+++ objetos.js
@@ -11,3 +11,28 @@ person["mom"] = "Dani";
 person.bf = "Neto";
 
 console.log(person)
+
+const lanches = {
+    cardapio: [
+        {nome: "x-bacon", preco: "R$ 9,00"},
+        {nome: "x-salada", preco: "R$ 7,50"}
+    ],
+
+    meuPedido: function (pedido){
+        return console.log(this.cardapio[pedido]);
+    },
+
+    //arrow function não funciona da mesma forma, pois ele não recebe um valor declarado antes dele
+
+    meuPedidoArrow: (pedido) => {
+        this.cardapio = [ //declarando o this no mesmo escopo da arrow
+            {nome: "x-bacon", preco: "R$ 9,00"},
+            {nome: "x-salada", preco: "R$ 7,50"}
+        ];
+
+        return console.log(this.cardapio[pedido])
+    }
+};
+
+lanches.meuPedido(0)
+lanches.meuPedidoArrow(0)
